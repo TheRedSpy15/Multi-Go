7ced670a39364019cc0a82027a465803af4396d3
Sean Prashad <S.Prashad@outlook.com>
2018-10-07 16:29:17 -0400

    Fixes #3: License indentation missing
    Corrects the indentation of the license headers for each .go source
    file.

--- MultiGo.go
+++ MultiGo.go
@@ -13,7 +13,7 @@ package main
 // TODO: add 'cleanTemp' task
 
 /*
-Copyright 2018 TheRedSpy15
+   Copyright 2018 TheRedSpy15
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ Copyright 2018 TheRedSpy15
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
-limitations under the License.
+   limitations under the License.
 */
 
 import (
