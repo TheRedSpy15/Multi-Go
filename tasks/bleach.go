2c75fe247fd3e8bbc63f8794ec19e5cb3e493e0d
TheRedSpy15 <hjadar15@gmail.com>
2020-01-17 16:36:42 -0500

    fixed license notices

--- tasks/bleach.go
+++ tasks/bleach.go
@@ -8,7 +8,7 @@ import (
 )
 
 /*
-   Copyright 2018 TheRedSpy15
+   Copyright 2020 TheRedSpy15
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
